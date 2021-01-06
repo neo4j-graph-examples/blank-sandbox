94a5fc3bd6f37c1e63a2782a4d8e306e0be25c33
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-01-06 11:06:18 +0000

    Updating sandbox blank-sandbox

---
+++ code/javascript/example.js
@@ -0,0 +1,29 @@
+// npm install --save neo4j-driver
+// node example.js
+const neo4j = require('neo4j-driver');
+const driver = neo4j.driver('bolt://<HOST>:<BOLTPORT>',
+                  neo4j.auth.basic('<USERNAME>', '<PASSWORD>'), 
+                  {/* encrypted: 'ENCRYPTION_OFF' */});
+
+const query =
+  `
+  MATCH (n)
+  RETURN COUNT(n) AS count
+  LIMIT $limit
+  `;
+
+const params = {"limit": "10"};
+
+const session = driver.session({database:"neo4j"});
+
+session.run(query, params)
+  .then((result) => {
+    result.records.forEach((record) => {
+        console.log(record.get('count'));
+    });
+    session.close();
+    driver.close();
+  })
+  .catch((error) => {
+    console.error(error);
+  });
