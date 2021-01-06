a12a0c7b7caa7622209dadcb5c7334e746464a96
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-01-06 11:06:18 +0000

    Updating sandbox blank-sandbox

---
+++ code/python/example.py
@@ -0,0 +1,24 @@
+# pip3 install neo4j-driver
+# python3 example.py
+
+from neo4j import GraphDatabase, basic_auth
+
+driver = GraphDatabase.driver(
+  "bolt://<HOST>:<BOLTPORT>", 
+  auth=basic_auth("<USERNAME>", "<PASSWORD>"))
+
+cypher_query = '''
+MATCH (n)
+RETURN COUNT(n) AS count
+LIMIT $limit
+'''
+
+with driver.session(database="neo4j") as session:
+  results = session.read_transaction(
+    lambda tx: tx.run(cypher_query,
+      limit="10").data())
+
+  for record in results:
+    print(record['count'])
+
+driver.close()
