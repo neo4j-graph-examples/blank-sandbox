37c1ae0685f4b9a79c9a78b9e604d715d970360e
Sandbox 🤖 <neo4j-oss-build@neotechnology.com>
2021-11-30 16:25:17 +0000

    Triggered by direct commit. Origin: https://github.com/neo4j-contrib/sandbox-code-updater/commit/acf246e73cc30b12092b13db364df208a2bcc766

--- code/python/example.py
+++ code/python/example.py
@@ -16,7 +16,7 @@ LIMIT $limit
 with driver.session(database="neo4j") as session:
   results = session.read_transaction(
     lambda tx: tx.run(cypher_query,
-                      limit=10).data())
+                      limit="10").data())
   for record in results:
     print(record['count'])
 
